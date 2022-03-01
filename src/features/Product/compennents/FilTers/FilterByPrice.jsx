0d48450e10eeb300ee9c4452c81e2f1bc879bc3d
Hoàng Nam <nnam44565@gmail.com>
2022-03-01 21:12:36 +0700

    price

--- src/features/Product/compennents/FilTers/FilterByPrice.jsx
+++ src/features/Product/compennents/FilTers/FilterByPrice.jsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
-
+import { useState ,useRef} from "react";
 const FilterByPrice = ({onChange}) => {
-
+    const inputRef = useRef()
+    const inputRef_lte = useRef()
     const [value, setValue] = useState({
         salePrice_gte:null,
         salePrice_lte:null
     });
-
 
     const handleChange = (event) => {
         setValue((prevState) => {
@@ -17,17 +16,24 @@ const FilterByPrice = ({onChange}) => {
         if(onChange){
             onChange(value);
         }
+        inputRef.current.value = '';
+        inputRef_lte.current.value = '';
+        setValue({
+            salePrice_gte:null,
+            salePrice_lte:null
+        });
     }
+
     return (  
         <>
             <h5>Chọn khoảng giá</h5>   
            <div className="row">
                 <div className="col-md-6">
-                <input type={'text'} name={'salePrice_gte'} value={value.salePrice_gte} onChange={handleChange}/>
+                <input ref={inputRef}  value={value.salePrice_gte} type={'text'} name={'salePrice_gte'} onChange={handleChange}/>
                 </div>
                 <span>-</span>
                 <div className="col-md-6">
-                <input type={'text'} name={'salePrice_lte'} value={value.salePrice_lte} onChange={handleChange}/>
+                <input ref={inputRef_lte} value={value.salePrice_lte} type={'text'} name={'salePrice_lte'} onChange={handleChange}/>
                 </div>
                 <div className="col-md-12 mt-2">
                 <button className="btn-primary p-1"
